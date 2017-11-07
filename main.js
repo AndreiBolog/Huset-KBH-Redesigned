56170fd52405d8558afb5676781c9b91beeeb9c1
AndreiBolog <bologandrei.92@gmail.com>
2017-11-07 15:11:27 +0100

    The Webpage fetching music events

--- main.js
+++ main.js
@@ -5,16 +5,24 @@ function getEvents(){
 }
 
 function showMusicEvent(data){
-    console.log(data);
+//    console.log(data);
     let list = document.querySelector("#list");
     let template = document.querySelector("#event-template").content;
 
     data.forEach(function(event){
-      console.log(event);
+//      console.log(event);
     let clone = template.cloneNode(true);
     let title = clone.querySelector("h1");
-    let execerpt =
+    let excerpt = clone.querySelector(".excerpt p");
+    let price = clone.querySelector(".price span");
+    let img =clone.querySelector("img");
+
+
         title.textContent = event.title.rendered;
+        excerpt.innerHTML = event.excerpt.rendered;
+        price.textContent = event.acf.price;
+        console.log(event._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
+        img.setAttribute("src", event._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
     list.appendChild(clone);
     })
 
